0725feb64e88b63f5df57b76e168bb843d248a08
André Moreira <andre-f-moreira@hotmail.com>
2023-11-04 14:06:18 +0000

    Bear Sound Added

--- AR project/Assets/Script/BearSound.cs
+++ AR project/Assets/Script/BearSound.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BearSound : MonoBehaviour
-{ 
+{
     public AudioSource BearAudio1;
     public AudioSource BearAudio2;
     public AudioSource BearAudio3;
@@ -11,24 +11,27 @@ public class BearSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(PlayBearSounds());
+    }
+
+    IEnumerator PlayBearSounds()
+    {
+        // Play the first sound
         BearAudio1.Play();
 
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        StartCoroutine(Wait());
-
-    }
-
-
-    IEnumerator Wait()
-    {
-
+        // Wait for 5 seconds
         yield return new WaitForSeconds(5);
-        BearAudio1.Play();
 
+        // Play the second sound after the delay
+        BearAudio2.Play();
+
+        // Wait for another 5 seconds
+        yield return new WaitForSeconds(5);
+
+        // Play the third sound after the second delay
+        BearAudio3.Play();
+
+        // Optional: If you want to loop the sounds, call the coroutine again
+        StartCoroutine(PlayBearSounds());
     }
 }
